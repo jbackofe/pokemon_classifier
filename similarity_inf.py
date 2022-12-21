b17b8fc2b87146a009d473531ee77c7cc635f145
Jared Backofen <backofen.jared@gmail.com>
2022-12-21 02:00:05 -0700

    Added argparse and logging callbacks

--- similarity_inf.py
+++ similarity_inf.py
@@ -1,10 +1,12 @@
 import cv2
 import os
-
-import numpy as np
-from tabulate import tabulate
-
 import gc
+import json
+import numpy as np
+
+import argparse
+import logging
+import sys
 
 # INFO messages are not printed.
 # This must be run before loading other modules.
@@ -22,6 +24,39 @@ tf.keras.backend.clear_session()
 
 print("TensorFlow:", tf.__version__)
 print("TensorFlow Similarity", tfsim.__version__)
+
+#Use this format (%Y-%m-%dT%H:%M:%SZ) to record timestamp of the metrics
+logging.basicConfig(
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%SZ",
+    level=logging.DEBUG)
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_dir', 
+                        type=str, 
+                        default='../models/pokemon_similarity',
+                        help='Name of the model folder.')
+    parser.add_argument('--data_dir',
+                        type=str,
+                        default='../test_images/xy12_test/Clefairy',
+                        help='Name of the inference data folder.')
+    parser.add_argument('--width',
+                        type=int,
+                        default=120,
+                        help='The width of the inference data.')
+    parser.add_argument('--height',
+                        type=int,
+                        default=185,
+                        help='The height of the inference data.')
+    parser.add_argument('--export_folder',
+                        type=str,
+                        default='./predictions',
+                        help='Folder to save prediction.')
+
+    args, _ = parser.parse_known_args(args=argv[1:])
+
+    return args
 
 def import_inf_data(path, dim):
     x = []
@@ -51,36 +86,55 @@ def import_inf_data(path, dim):
 
     return x
 
-save_path = "../models/pokemon_similarity"  # @param {type:"string"}
+# Load the model and index
+def load_model(model_path):
+    # reload the model
+    reloaded_model = tf.keras.models.load_model(
+        model_path,
+        custom_objects={"SimilarityModel": tfsim.models.SimilarityModel},
+    )
+    # reload the index
+    reloaded_model.load_index(model_path)
 
-# reload the model
-reloaded_model = tf.keras.models.load_model(
-    save_path,
-    custom_objects={"SimilarityModel": tfsim.models.SimilarityModel},
-)
-# reload the index
-reloaded_model.load_index(save_path)
+    return reloaded_model
 
-# Import the data
-path = '../test_images/xy12_test/Clefairy'
-dim = (120, 185)
-x_inf = import_inf_data(path, dim)
+def load_class_info():
+    # Import class_names
+    with open('class_names.npy', 'rb') as f:
+        class_names = np.load(f)
 
-# Import class_names
-with open('class_names.npy', 'rb') as f:
-    class_names = np.load(f)
+    # Import classes
+    with open('classes.npy', 'rb') as f:
+        classes = np.load(f)
 
-# Import classes
-with open('classes.npy', 'rb') as f:
-    CLASSES = np.load(f)
+    # Create class labels
+    labels = np.append(classes, "Unknown")
 
-# Save labels, cutpoint from training
-labels = np.append(CLASSES, "Unknown")
-print(len(labels))
-output = reloaded_model.match(x_inf, cutpoint="optimal", no_match_label=len(labels)-1)
-labels[output]
-for val in output:
-    if val > len(class_names)-1:
-        print('Unknown')
-    else:
-        print(class_names[val])
+    return labels, class_names
+
+def inference(argv=None):
+    args = parse_arguments(sys.argv if argv is None else argv)
+
+    reloaded_model = load_model(args.model_dir)
+    labels, class_names = load_class_info()
+    x_inf = import_inf_data(args.data_dir, (args.width, args.height))
+
+    # Get the prediction
+    pred = reloaded_model.match(x_inf, cutpoint="optimal", no_match_label=len(labels)-1)
+
+    # Get the predicted class name
+    outputs = []
+    for val in pred:
+        if val > len(class_names)-1:
+            outputs.append('Unknown')
+        else:
+            outputs.append(class_names[val])
+    
+    print(outputs)
+    # Save outputs
+    with open(f'{args.export_folder}/output.txt', 'w') as filehandle:
+        json.dump(outputs, filehandle)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    inference()
